39df8d7fbe66dfcd0aa375b4c9f960f9dababdf0
Santeri Toikka <586403+sjtoik@users.noreply.github.com>
2020-09-09 12:50:00 +0300

    Updated example.

--- bin/standalone-run.sh
+++ bin/standalone-run.sh
@@ -2,4 +2,6 @@
 # Configuration of connect component is 2 parted. First you need to setup the basic worker connectivity to Kafka server.
 # That is the first .properties file. Secondly, the desired connect feature needs to be configured. Here, the
 # configuration is done in the latter file having the application specific configurations.
-./standalone/confluent-5.5.1/bin/connect-standalone ../config/compose-connect-standalone-worker.properties ../config/compose-connect-json.properties
+
+# wait for the services to come up: docker-compose up -d zookeeper broker
+./standalone/confluent-5.5.1/bin/connect-standalone ../config/standalone-worker.properties ../config/standalone-connect-json.properties
