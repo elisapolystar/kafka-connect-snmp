a8a97f9aa509d3006c0c65686f49a7bc1d5da788
johannessarpola <johannes.sarpola@gmail.com>
2021-09-17 10:25:36 +0300

    Update the new class copyright to match licensing definitions.

--- src/main/java/com/github/jcustenborder/kafka/connect/snmp/AuthenticationProtocol.java
+++ src/main/java/com/github/jcustenborder/kafka/connect/snmp/AuthenticationProtocol.java
@@ -1,6 +1,5 @@
 /**
  * Copyright © 2021 Elisa Oyj
- * Copyright © 2017 Jeremy Custenborder (jcustenborder@gmail.com)
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
